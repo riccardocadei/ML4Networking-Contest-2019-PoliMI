f51545abaf8a5f6a99072c7e1cc655115205f093
Riccardo Cadei <47679062+RiccardoCadei@users.noreply.github.com>
2019-05-16 19:44:23 +0200

    Add files via upload

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,13 +1,18 @@
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score
 import numpy
 
 def DecisionTreeA(X_train, y_train, X_test, User_Id):
-    dt = DecisionTreeClassifier()
+    dt = DecisionTreeClassifier(max_depth=9)
     clf = dt.fit(X_train, y_train)
-    print('Accuracy of Decision Tree classifier on training set: {:.2f}'
-         .format(clf.score(X_train, y_train)))
+    scores = cross_val_score(dt, X_train, y_train, cv = 10, scoring='precision')
+    print(f"Mean crossvalidation Micro-F1: {numpy.mean(scores):.3f}")
 
+    # print('Accuracy of Decision Tree classifier on training set: {:.2f}'.format(clf.score(X_train, y_train)))
+
+    print('ciaone')
+     
     pred = dt.predict(X_test)
     pred1 = []
     a = 0
@@ -15,7 +20,7 @@ def DecisionTreeA(X_train, y_train, X_test, User_Id):
         pred1.append([int(User_Id[a]),int(i)])
         a += 1
     
-    numpy.savetxt("predictions\\dtpredz.csv", pred1, fmt='%10i',delimiter=",")
+    numpy.savetxt("predictions\\dtpredgs.csv", pred1, fmt='%10i',delimiter=",")
 
     
 
